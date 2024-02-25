74a007d7a683dd483364bba749a4ccb504b5817c
purushotham123352 <86943197+purushotham123352@users.noreply.github.com>
2024-02-25 13:50:45 +0530

    second_classmate

---
+++ classmate/src/main/java/com/example/classmate/ClassmateApplication.java
@@ -0,0 +1,14 @@
+package com.example.classmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmateApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ classmate/src/test/java/com/example/classmate/ClassmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
