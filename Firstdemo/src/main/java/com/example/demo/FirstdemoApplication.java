4eca1baf71820166f22b77045d21df3e720d5b5e
purushotham123352 <86943197+purushotham123352@users.noreply.github.com>
2024-02-26 10:36:15 +0530

    secondcommit

---
+++ Firstdemo/src/main/java/com/example/demo/FirstdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstdemoApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
